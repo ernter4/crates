b7ace4c32298e4489c60b5c57dee102246219182
paul <harburg.schroeppel@gmail.com>
2026-02-07 16:00:05 +0100

    chore(main): release 0.2.5

--- app/accounting/routers/router.py
+++ app/accounting/routers/router.py
@@ -1,8 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Header, HTTPException, Depends
 from .customer import  router as customers_router
 from .invoice import router as invoices_router
 from  .export import router as export_router
-router = APIRouter()
+
+from ...shared.auth import authorize
+from app.shared.dependencies import  CurrentUserDep
+
+
+def auth(user:CurrentUserDep):
+    return authorize(["admin"],user)
+router = APIRouter(
+
+    dependencies=[Depends(auth)]
+)
 router.include_router(customers_router, prefix="/customer")
 router.include_router(  invoices_router,prefix="/invoice")
 router.include_router(export_router, prefix="/export")
