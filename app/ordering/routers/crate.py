3cc62b5fe57f40b3cb9154c43a9c063b29a5129c
paul <harburg.schroeppel@gmail.com>
2026-03-15 19:01:37 +0100

    fix: added missing files

---
+++ app/ordering/routers/crate.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+from sqlmodel import select
+
+from app.ordering.models import Crate
+from app.shared.dependencies import SessionDep
+from app.shared.update_database import update_database
+
+router = APIRouter()
+
+@router.get("/")
+def get_crates(session :SessionDep):
+    return session.exec(select(Crate)).fetchall()
+@router.post("/")
+def create_crate(crate :Crate, session : SessionDep):
+    update_database(crate, session)
+    return crate
+@router.put("/{crate_id}")
+def update_crate( crate_id :int, session : SessionDep):
+    crate = session.get(Crate,crate_id)
+    update_database(crate, session)
