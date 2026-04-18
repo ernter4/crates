cfc83ad414fbbe8156613954d73a8ea130a39f73
paul <harburg.schroeppel@gmail.com>
2026-04-18 17:48:41 +0200

    feat added to close orders at crate last seen update

--- app/ordering/services/models/CrateService.py
+++ app/ordering/services/models/CrateService.py
@@ -1,10 +1,27 @@
-from sqlmodel import Session
+from datetime import datetime
+from typing import Optional
 
-from app.ordering.models import CrateWithID, Crate
-from app.shared.ModelService import ModelService
+from sentry_sdk import session
+from sqlmodel import Session, select
 
+from app.ordering.models import CrateWithID, Crate, Order, OrderWithID
+from app.ordering.services.models.order import OrderService
+from app.shared.ModelService import ModelService, TUpdate, Tout
 
 
 class CrateService(ModelService[Crate,Crate,CrateWithID,CrateWithID]):
     def __init__(self, session: Session):
         super().__init__(session, Crate,CrateWithID)
+    def update(self, data: TUpdate,item_id:Optional[int]= None ) -> CrateWithID:
+
+        statement = select(Order).where(Order.return_date == None)
+        if item_id:
+            statement = statement.where(Order.crate_id == item_id)
+        else:
+            statement = statement.where(Order.crate_id == data.id)
+        order:OrderWithID = self.session.exec(statement).first()
+        if order:
+            order.return_date = datetime.now()
+            OrderService(self.session,self.current_user).update(order)
+
+        return super().update(data,item_id)
